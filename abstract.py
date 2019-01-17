db66d6b84645b6034f7c7a0f3ab668147b94009d
Daniel Xu <niellxu@yahoo.ca>
2019-01-17 14:23:04 -0500

    Added abstract token, domain and working on abstract tree building

--- abstract.py
+++ abstract.py
@@ -2,6 +2,48 @@
 This module contains the abstract level of the syntax tree,
 it is real abstract syntax tree
 """
+import re
+
+
+class AbstractTree():
+    def __init__(self, associativity, root=None):
+        """
+        associativity: The associativity of the expression, this
+        is not the same as the one in connective. This defines the
+        whole rule of building tree.
+        """
+        self.root = root
+        self.cur = None
+    
+    def _left_append(self, n):
+        pass
+    
+    def _right_append(self, n):
+        pass
+
+
+class AbstractToken():
+    def __init__(self, sym, is_value=False, is_func=False,
+                    is_oper=False,
+                    is_leftb=False, is_rightb=False):
+        self.sym = sym
+        self.is_value = is_value
+        self.is_func = is_func
+        self.is_oper = is_oper
+        self.is_leftb = is_leftb
+        self.is_rightb = is_rightb
+    
+    def __str__(self):
+        return "AbstractToken(sym=" + str(self.sym) +\
+                    ", is_func=" + str(self.is_func) +\
+                    ", is_value=" + str(self.is_value) +\
+                    ", is_oper=" + str(self.is_oper) +\
+                    ", is_leftb=" + str(self.is_leftb) +\
+                    ", is_rightb=" + str(self.is_rightb) +\
+                    ")"
+    
+    def __repr__(self):
+        return self.__str__()
 
 
 class AbstractNode():
@@ -30,17 +72,188 @@ class Connective():
 
     is_binary: True if it is binary connective, such as '1+1' in math
 
-    is_unary_func: True if it is a unary special function, such as 'sin(x)'
-
-    is_binary_func: True if it is a binary special function, such as 'max(1, 2)'
+    call=None: The lambda, it can be understanded as how this connective
+    calculate, default is None, which means this connective can not be
+    used in calculation
     """
     def __init__(self, symbol, precedence, associativity,
-                    is_unary, is_binary,
-                    is_unary_func, is_binary_func):
+                    is_unary, is_binary, call=None):
         self.symbol = symbol
         self.precedence = precedence
         self.associativity = associativity
         self.is_unary = is_unary
         self.is_binary = is_binary
-        self.is_unary_func = is_unary_func
-        self.is_binary_func = is_binary_func
+
+
+class Function():
+    """
+    Function is a function in expression. For example, in  math, max(a,b)
+    is a binary function which return the greater number, sin(x) is a function
+    that return the trig value.
+
+    is_unary: True if it is unary function, such as sin(0) in math
+
+    is_binary: True if it is a binary function, such as max(1, 2) in math
+
+    call=None: The lambda, it can be understanded as how this function
+    calculate, default is None, which means this function can not be
+    used in calculation
+    """
+    def __init__(self, symbol, is_unary, is_binary, call=None):
+        self.symbol = symbol
+        self.is_unary = is_unary
+        self.is_binary = is_binary
+
+
+class Domain():
+    """
+    Domain of the expression, for example, in math, the domain is all real
+    numbers. In boolean expression, the domain is either True or False.
+    getDomain() method provide handy domain such as real numbers.
+
+    d=real: Domain of the expression, default is 'real', which represents
+    real numbers. It can also be a list, all elements inside the list will
+    represent the domain
+    """
+    def __init__(self, d='real'):
+        self.domain = d
+    
+    def in_domain(self, v):
+        """
+        Test if the given value is in the domain, return True if yes, False
+        otherwise. Please notice that all values are represented by str format
+        """
+        if self.domain == 'real':
+            return self._is_real(v)
+        else:
+            return v in self.domain
+
+    def _is_real(self, v):
+        "Internal use, return True if given str is value"
+        try:
+            float(v)
+            return True
+        except ValueError:
+            return False
+
+
+class AbstractBuilder():
+    """
+    This class help users to build up the abstract trees
+    """
+    def __init__(self, d=Domain()):
+        self.function_mapper = {}
+        self.domain = d
+        self.connects_mapper = {}
+    
+    def add_connect(self, con):
+        self.connects_mapper[con.symbol] = con
+    
+    def add_func(self, func):
+        self.function_mapper[func.symbol] = func
+    
+    def build(self, e):
+        """
+        Build the Abstract tree by given expression, this method
+        should be called when every connectives had been setup
+        """
+    
+    def _post(self, e):
+        tokens = self._tokenize(e)
+        q = []
+        op = []
+        index = 0
+        while index < len(tokens):
+            token = tokens[index]
+            if token.is_value:
+                q.append(token)
+            if token.is_leftb:
+                op.append(token)
+            elif token.is_oper:
+                if len(op) > 0:
+                    while len(op) > 0 and (op[-1].is_func or (op[-1].sym != "(" and self._has_precedence(op[-1].sym, token.sym))):
+                        q.append(op.pop())
+                    op.append(token)
+                else:
+                    op.append(token)
+            elif token.is_rightb:
+                while len(op) > 0 and op[-1].sym != "(":
+                    q.append(op.pop())
+                op.pop()
+            elif token.is_func:
+                op.append(token)
+            index += 1
+        while len(op) > 0:
+            q.append(op.pop())
+        return [x.sym for x in q]
+    
+    def _has_precedence(self, a, b):
+        return ((b.associativity == 'right' and
+             a.precedence > b.precedence) or
+            (b.associativity == 'left' and
+             a.precedence >= b.precedence))
+
+    def _tokenize(self, e):
+        index = 0
+        eindex = 0
+        result = []
+        starts, ends = self._match_regex(self._func_regex(), e)
+        while index < len(e):
+            if index in starts:
+                result.append(AbstractToken(e[index: ends[eindex]], is_func=True))
+                index = ends[eindex]
+                eindex += 1
+                continue
+            t = e[index]
+            if self.domain.in_domain(t):
+                t, index = self._sub_val(index, e)
+                result.append(t)
+            elif t == "(":
+                result.append(AbstractToken(t, is_leftb=True))
+            elif t == ")":
+                result.append(AbstractToken(t, is_rightb=True))
+            elif self.is_connect(t):
+                result.append(AbstractToken(t, is_oper=True))
+            index += 1
+        return result
+    
+    def is_connect(self, c):
+        return c in self.connects_mapper
+
+    def _sub_val(self, index, e, prev=""):
+        d = prev
+        while index < len(e) and self.domain.in_domain(e[index]):
+            d += e[index]
+            index += 1
+        index-=1
+        return AbstractToken(d, is_value=True), index
+    
+    def _func_regex(self):
+        "Construct regex for special math functions"
+        regex = "("
+        for f in self.function_mapper:
+            if "(" in f:
+                index = f.find("(")
+                regex += f[:index] + "\\" + f[index:] + "|"
+            else:
+                regex += f + "|"
+        return regex[:-1] + ")"
+    
+    def _match_regex(self, r, e):
+        "Match the functions regex and return a list of start indices and a list of end indices"
+        reg = re.compile(r)
+        starts, ends = [], []
+        for m in reg.finditer(e):
+            starts.append(m.start())
+            ends.append(m.end())
+        return starts, ends
+
+
+
+fun = Function("abc", True, False, call=lambda x:x+2)
+mul = Connective("*", 2, 'right', False, True, call=lambda x,y:x*y)
+builder = AbstractBuilder()
+builder.add_connect(mul)
+builder.add_func(fun)
+
+print(builder._post("abc(3*2)"))
--- abstract_tree.py
+++
@@ -1,5 +0,0 @@
-"""
-This module intended to bring tree to a real abstract
-level. As long as the rules of the trees are defined,
-it can build any tree such as boolean or math.
-"""
